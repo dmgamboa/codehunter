3aa7ad1902d2beb94180d9852060b4658b10df8e
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-04-29 22:01:27 -0700

    Setup routing for signup and login. Basic syntax changes.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
-import { Switch } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import "./App.less";
 import { getRoute } from "./context/routers";
 import MainLayout from "./components/Layout";
 import SampleFeature from "./feature/sample-feature";
 import Signup from "./components/Signup/Signup";
+import Profile from "./components/Profile/Profile";
+import Login from "./components/Login/Login";
 import AuthProvider from "./context/Auth"
 
 function App() {
   return (
-    <AuthProvider>
-      <Signup />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/" component={Profile} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
--- client/src/context/Auth.js
+++ client/src/context/Auth.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { auth } from '../firebase'
+import React, { useContext, useState, useEffect } from "react";
+import { auth } from "../firebase"
 
 const Auth = React.createContext();
 
