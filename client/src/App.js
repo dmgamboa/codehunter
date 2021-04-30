5199b9295f2e293598d2695a5f01689694cbd55c
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-04-30 10:00:27 -0700

    File structure updates

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,9 @@ import "./App.less";
 import { getRoute } from "./context/routers";
 import MainLayout from "./components/Layout";
 import SampleFeature from "./feature/sample-feature";
-import Signup from "./components/Signup";
-import Profile from "./components/Profile";
-import Login from "./components/Login";
+import Signup from "./feature/signup";
+import Profile from "./feature/profile";
+import Login from "./feature/login";
 import AuthProvider from "./context/Auth";
 import PrivateRoute from "./components/PrivateRoute";
 
