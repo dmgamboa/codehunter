7b76f295d994825e7700b5ea3195a93a37064864
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-05-02 16:21:50 -0700

    Added sample GET request. POST doesn't work yet.

--- server/app.js
+++ server/app.js
@@ -1,44 +1,88 @@
 // Our application entry point
+// Express stuff
+const express = require('express');
+const app = express();
+const port = 8000;
+app.use("axios", express.static("../client/src/pages/sample-feature"));
+
+// MongoDB stuff
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://teamfourcodehunter:cityofvancouver@codecluster.ktv41.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://teamfourcodehunter:cityofvancouver@codecluster.ktv41.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // The database to use
-const dbName = "other";
+const dbName = "test";
 
-async function run() {
-  try {
-    await client.connect();
-    console.log("Connected correctly to server");
-    const db = client.db(dbName);
-
-    // Use the collection "people"
-    const col = db.collection("people");
-
-    // Construct a document                                                                                                                                                              
-    let personDocument = {
-      "name": {
-        "first": "Alan",
-        "last": "Turing"
-      },
-      "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
-      "death": new Date(1954, 5, 7), // June 7, 1954                                                                                                                                  
-      "contribs": ["Turing machine", "Turing test", "Turingery"],
-      "views": 1250000
+// Call when retrieving data.
+/*app.get("/example", (req, res) => {
+  async function run() {
+    try {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db(dbName);
+  
+      // Use the collection "people"
+      const col = db.collection("people");
+  
+      // Construct a document                                                                                                                                                              
+      let personDocument = {
+        "name": {
+          "first": "Alan",
+          "last": "Turing"
+        },
+        "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
+        "death": new Date(1954, 5, 7), // June 7, 1954                                                                                                                                  
+        "contribs": ["Turing machine", "Turing test", "Turingery"],
+        "views": 1250000
+      }
+  
+      // Insert a single document, wait for promise so we can read it back
+      //const p = await col.insertOne(personDocument);
+      // Find one document
+      const myDoc = await col.findOne();
+      // Send it back
+      res.send(myDoc);
+      
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
     }
-
-    // Insert a single document, wait for promise so we can read it back
-    const p = await col.insertOne(personDocument);
-    // Find one document
-    const myDoc = await col.findOne();
-    // Print to the console
-    console.log(myDoc);
-
-  } catch (err) {
-    console.log(err.stack);
-  } finally {
-    await client.close();
   }
-}
+  
+  run().catch(console.dir);
+});*/
 
-run().catch(console.dir);
+app.get("/sampleGet", (req, res) => {
+  console.log("In server-side sampleGet");
+  const stuff = {id: 1};
+  res.send(stuff);
+});
+
+app.post("/samplePost", (req, res) => {
+  console.log("In server-side samplePost. Req: ", req.firstName);
+  //let request = req.query["format"];
+
+  
+
+  /*async function run() {
+    try {
+      await client.connect();
+      const db = client.db(dbName);
+
+      const profiles = db.collection("profiles");
+      // create a document to be inserted
+      const doc = req;
+      const result = await profiles.insertOne(doc);
+      res.send(result);
+
+    } finally {
+      await client.close();
+    }
+  }
+  run().catch(console.dir);*/
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
