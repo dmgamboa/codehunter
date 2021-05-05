a034405a52fa5a90ba68b009e27417457f049a1c
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-05-05 12:48:23 -0700

    add jsx elements for walkthrough

---
+++ client/src/pages/Walkthrough/WalkthroughScreen/constant.js
@@ -0,0 +1,21 @@
+import walkthroughImage01 from "../../../assets/icons/walkthrough/walkthrough-01.png";
+import walkthroughImage02 from "../../../assets/icons/walkthrough/walkthrough-01.png";
+import walkthroughImage03 from "../../../assets/icons/walkthrough/walkthrough-01.png";
+
+export const carouselItems = [
+    {
+        img: walkthroughImage01,
+        heading: "Hunt Codes",
+        body: "Blahbbity Blihbbity Bloo"
+    },
+    {
+        img: walkthroughImage02,
+        heading: "Redeem Rewards",
+        body: "Blahbbity Blihbbity Bloo"
+    },
+    {
+        img: walkthroughImage03,
+        heading: "Add Friends",
+        body: "Blahbbity Blihbbity Bloo"
+    },
+];
--- client/src/pages/Walkthrough/WalkthroughScreen/index.js
+++ client/src/pages/Walkthrough/WalkthroughScreen/index.js
@@ -1,6 +1,37 @@
+import { Carousel, Button } from "antd-mobile";
+
+import { carouselItems } from "./constant";
+import { Image } from "./styled";
+
 const WalkthroughScreen = () => {
+    
+    const renderCarouselItems = (items) => {
+        return items.map(({ img, heading, body }) => {
+            return (
+                <>
+                    <img
+                        src={img}
+                        onLoad={() => window.dispatchEvent(new Event("resize"))}
+                    />
+                    <h1>{heading}</h1>
+                    <p>{body}</p>
+                </>
+            );
+        })
+    }
+
     return (
-        <span>This be the walkthrough screen...</span>
+        <>
+            <Carousel autoplay={false}>
+                {renderCarouselItems(carouselItems)}
+            </Carousel>
+
+            <Button type="primary">
+                Join the hunt
+            </Button>
+
+            <span className="login">Log In</span>
+        </>
     );
 }
 
