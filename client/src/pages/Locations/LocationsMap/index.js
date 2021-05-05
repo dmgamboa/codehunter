b5f62397fed8166927232ab6ad9346fe3394ab5a
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-05-05 13:14:30 -0700

    Updated map integration

--- client/src/pages/Locations/LocationsMap/index.js
+++ client/src/pages/Locations/LocationsMap/index.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { GoogleMap, withScriptjs } from "react-google-maps";
+import GoogleMapReact from "google-map-react";
+
+const vancouverCoords = {
+  lat: 49.246292,
+  lng: -123.116226
+};
+
+const initialZoom = 13;
 
 const LocationsMap = () => {
-    return (
-        <GoogleMap
-            defaultZoom={10}
-            defaultCenter={{ lat: 12, lng: 12 }}
-        />
-    );
-}
-
-const MapContainer = () =>
+  return (
+    // Important! Always set the container height explicitly
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAeyRbNMuUdpdvj8bd5CTx90-vOrVl2flM" }}
+        center={{ lat: 49.246292, lng: -123.116226 }}
+        zoom={initialZoom}
+      />
+    </div>
+  );
+};
 
 export default LocationsMap;
