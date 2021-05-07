1a76f95d6a6e83d938bf745fd7fdc90cc3697a7f
Ethan Lee <33e.lee@gmail.com>
2021-05-07 12:22:31 -0700

    Fixed bug with axios responding with data

--- server/api-routes/registration.route.js
+++ server/api-routes/registration.route.js
@@ -4,10 +4,10 @@ const router = express.Router();
 
 // /registratrion/create/userDoc
 // /registration (in app.js)
-router.route("/create/userDoc")
+router.route("/")
     .post((req, res, next) => {
+        console.log("api route: ", req.body)
         res.send(req.body)
-    })
-
+    });
 
 export default router;
