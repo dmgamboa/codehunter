a92edaa218f6ddbe05a89171b2fde2ee46cb7d03
PariyaKasaeian <pkasaiyan@gmail.com>
2021-05-08 01:12:23 -0700

    Displays avatar and point section to the profile page screen.

--- client/src/pages/Profile/ProfileScreen/index.js
+++ client/src/pages/Profile/ProfileScreen/index.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from "react";
 // import { useAuth } from "../../context/Auth";
 import { useHistory } from "react-router-dom";
+import AvatarSection from "../components/Avatar/AvatarSection"
+import PointsSection from "../components/Points/PointsSection"
 
 const ProfileScreen = () => {
 //   const { user, logout } = useAuth();
@@ -13,6 +15,8 @@ const ProfileScreen = () => {
 
   return (
     <div>
+      <AvatarSection/>
+      <PointsSection points={12}/>
       <h1>Profile page</h1>
       Email: Some email
       <button onClick={handleLogout}>Log Out</button>
