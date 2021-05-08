06a6585c35b67c579f9637817bd06decfdeb9e02
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-05-08 13:38:19 -0700

    Added a simple QR code scanner

--- client/src/pages/Locations/LocationsMap/index.js
+++ client/src/pages/Locations/LocationsMap/index.js
@@ -8,79 +8,82 @@ import { StyledMap } from "./styled";
 import { getMapData } from "../axios";
 
 const initialCoords = {
-  lat: 49.246292,
-  lng: -123.116226
+    lat: 49.246292,
+    lng: -123.116226,
 };
 
 const initialZoom = 13;
 
 const LocationsMap = () => {
-  const [markers, setMarkers] = useState([]);
-  const [coords, setCoords] = useState(initialCoords);
-  const mapsKey = process.env.REACT_APP_MAPS_KEY;
+    const [markers, setMarkers] = useState([]);
+    const [coords, setCoords] = useState(initialCoords);
+    const mapsKey = process.env.REACT_APP_MAPS_KEY;
 
-  const bound = 0.015;
-  
-  useEffect(async () => {
-    // UNCOMMENT TO USE CURRENT POSITION
-    //getCoords();
-    const mapData = await getMapData();
-    setMarkers(mapData.data);
-  }, []);
+    const bound = 0.015;
 
-  const getCoords = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        setCoords({
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude
+    useEffect(async () => {
+        // UNCOMMENT TO USE CURRENT POSITION
+        //getCoords();
+        const mapData = await getMapData();
+        setMarkers(mapData.data);
+    }, []);
+
+    const getCoords = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((pos) => {
+                setCoords({
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude,
+                });
+            });
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    };
+
+    const handleMarkerClick = (cultural_space_name) => {
+        console.log("This place is: ", cultural_space_name);
+    };
+
+    const renderMarkers = (markerData) => {
+        return markerData.map((code) => {
+            const { _id } = code;
+            const { cultural_space_name, local_area } = code.fields;
+            const { coordinates } = code.fields.geom;
+            if (
+                coords.lat > coordinates[1] - bound &&
+                coords.lat < coordinates[1] + bound &&
+                coords.lng > coordinates[0] - bound &&
+                coords.lng < coordinates[0] + bound
+            ) {
+                return (
+                    <Marker
+                        key={_id}
+                        lat={coordinates[1]}
+                        lng={coordinates[0]}
+                        handleClick={handleMarkerClick}
+                        cultural_space_name={cultural_space_name}
+                        local_area={local_area}
+                    />
+                );
+            }
         });
-      });
-    } else {
-      console.log("Geolocation is not supported by this browser.");
-    }
-  };
+    };
 
-  const handleMarkerClick = (cultural_space_name) => {
-    console.log("This place is: ", cultural_space_name);
-  };
-
-  const renderMarkers = (markerData) => {
-    return markerData.map(code => {
-      const { _id } = code;
-      const { cultural_space_name, local_area } = code.fields;
-      const { coordinates } = code.fields.geom;
-      if (coords.lat > coordinates[1] - bound
-        && coords.lat < coordinates[1] + bound
-        && coords.lng > coordinates[0] - bound
-        && coords.lng < coordinates[0] + bound) {
-        return (
-          <Marker
-            key={_id}
-            lat={coordinates[1]}
-            lng={coordinates[0]}
-            handleClick={handleMarkerClick}
-            cultural_space_name={cultural_space_name}
-            local_area={local_area}
-          />
-        );
-      }
-    });
-  };
-
-  return (
-    <StyledMap>
-      {//<SearchBar />
-      }
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: mapsKey }}
-        center={initialCoords}
-        zoom={initialZoom}
-      >
-        {renderMarkers(markers)}
-      </GoogleMapReact>
-    </StyledMap>
-  );
+    return (
+        <StyledMap>
+            {
+                //<SearchBar />
+            }
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: mapsKey }}
+                center={initialCoords}
+                zoom={initialZoom}
+            >
+                {renderMarkers(markers)}
+            </GoogleMapReact>
+        </StyledMap>
+    );
 };
 
 export default LocationsMap;
