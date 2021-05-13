6117f6f3ecf3bd82bf58d5a60a7abbc5adc204a8
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-05-12 20:54:58 -0700

    add form elements

--- client/src/pages/Locations/LocationsFilters/index.js
+++ client/src/pages/Locations/LocationsFilters/index.js
@@ -1,10 +1,26 @@
-import Icon from "@ant-design/icons";
+import { 
+    Form,
+    Radio,
+    Switch
+} from "antd";
+import Icon, { 
+    EnvironmentOutlined,
+    ArrowUpOutlined,
+    ArrowDownOutlined,
+    CheckCircleOutlined
+} from "@ant-design/icons";
 
 import { ReactComponent as FilterIcon } from "../../../assets/icons/filters.svg";
+import { ReactComponent as BookmarkIcon } from "../../../assets/icons/bookmark.svg";
 
 import StyledDrawer from "./styled";
+import { fromPairs } from "lodash";
 
-const LocationsFilters = ({ visible, onClose }) => {
+const LocationsFilters = ({
+    visible,
+    onClose,
+    filters
+}) => {
     return (
         <StyledDrawer
             placement="bottom"
@@ -18,7 +34,53 @@ const LocationsFilters = ({ visible, onClose }) => {
                 </>
             )}
         >
-            Hello!
+            <Form
+                initialValues={filters}
+                layout="vertical"
+            >
+                <Form.Item
+                    label="Sort by"
+                    name="sort"
+                >
+                    <Radio.Group className="sort">
+                        <Radio.Button value="az">
+                            <ArrowUpOutlined />
+                            A - Z
+                        </Radio.Button>
+
+                        <Radio.Button value="distance">
+                            <EnvironmentOutlined />
+                            Nearest
+                        </Radio.Button>
+                        
+                        <Radio.Button value="za">
+                            <ArrowDownOutlined />
+                            Z - A
+                        </Radio.Button>
+                    </Radio.Group>   
+                </Form.Item>
+
+                <Form.Item label="Show only:">
+                    <Form.Item name="bookmarked">
+                        <Switch />
+                        <span>
+                            <Icon component={BookmarkIcon} />
+                            Bookmarked
+                        </span>
+                    </Form.Item>
+
+                    <Form.Item name="visited">
+                        <Switch />
+                        <span>
+                            <CheckCircleOutlined />
+                            Visited
+                        </span>
+                    </Form.Item>
+
+                </Form.Item>
+                     
+   
+            </Form>
         </StyledDrawer>
     );
 }
--- client/src/pages/Locations/LocationsFilters/styled.js
+++ client/src/pages/Locations/LocationsFilters/styled.js
@@ -25,6 +25,35 @@ const StyledDrawer = styled(Drawer)`
             }
         }
     }
+
+    .sort {
+        display: flex;
+        width: 100%;
+
+        .ant-radio-button-wrapper {
+            flex: 1;
+
+            &:first-of-type {
+                border-top-left-radius: 1.5rem;
+                border-bottom-left-radius: 1.5rem;
+            }
+
+            &:last-of-type {
+                border-top-right-radius: 1.5rem;
+                border-bottom-right-radius: 1.5rem;
+            }
+
+            &.ant-radio-button-wrapper-checked {
+                background: ${colors.primary};
+                color: #FFF;
+                font-weight: bold;
+            }
+
+            .anticon {
+                margin-right: 0.5rem;
+            }
+        }
+    }
 `;
 
 export default StyledDrawer;
