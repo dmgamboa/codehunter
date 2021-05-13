391ed740f122b44a1cd35ce4f706d0f3f8037cdc
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-05-13 13:55:41 -0700

    refactor locationDetails into locationScreen

--- client/src/pages/Locations/LocationsList/index.js
+++ client/src/pages/Locations/LocationsList/index.js
@@ -1,42 +1,16 @@
-import { useState, useEffect } from "react";
-import Icon, { CloseCircleOutlined, EllipsisOutlined } from "@ant-design/icons";
+import Icon, { EllipsisOutlined } from "@ant-design/icons";
 
 import { ReactComponent as GoogleMapsIcon } from "../../../assets/icons/google-maps.svg";
 import { ReactComponent as BookmarkIcon } from "../../../assets/icons/bookmark.svg";
 
 import LocationCard from "../../../components/LocationCard";
-import LocationDetails from "../../../components/LocationDetails";
 
 
-const LocationsList = ({ locations }) => {    
-    const [detailsVisible, setDetailsVisible] = useState(false);
-
-
-
-    const handleDetailsOpen = () => {
-        setDetailsVisible(true);
-    }
-
-    const handleDetailsClose = () => {
-        setDetailsVisible(false);
-    }
-
-    const handleTabs = (tab) => {
-        switch (tab) {
-            case "directions":
-                // Redirect to GMaps
-                break;
-            case "bookmark":
-                // Add to Bookmarks
-                break;
-            case "details":
-                setDetailsVisible(true);
-                break;
-            case "close":
-                setDetailsVisible(false);
-        }
-    }
-
+const LocationsList = ({
+    locations,
+    handleTabs,
+    handleDetailsOpen
+}) => {    
     const testLocations = [{
         name: "Telus World of Science",
         distance: 5,
@@ -64,52 +38,6 @@ const LocationsList = ({ locations }) => {
         },
     ];
 
-    const testData = {
-        name: "Telus World of Science",
-        distance: 5,
-        bookmarked: true,
-        visited: false,
-        details: {
-            type: "Museum / Gallery",
-            address: "1455 Quebec St, Vancouver, BC V6A 3Z7",
-            hours: {
-                status: "Open now",
-                days: {
-                    mon: ["08:00 AM", "10:00 PM"],
-                    tue: ["08:00 AM", "10:00 PM"],
-                    wed: ["08:00 AM", "10:00 PM"],
-                    thu: ["08:00 AM", "10:00 PM"],
-                    fri: ["08:00 AM", "10:00 PM"],
-                    sat: [],
-                    sun: [],
-                }
-            },
-            phone: "6041234567",
-            website: "https://www.scienceworld.ca"
-        }
-    }
-
-    const detailsTabs = [
-        {
-            key: "directions",
-            name: "Get Directions",
-            icon: <Icon component={GoogleMapsIcon}/>,
-            onPress: handleTabs
-        },
-        {
-            key: "bookmark",
-            name: `${testData.bookmarked ? "Remove from" : "Add to"} Bookmarks`,
-            icon: <Icon component={BookmarkIcon}/>,
-            onPress: handleTabs
-        },
-        {
-            key: "close",
-            name: "Close",
-            icon: <CloseCircleOutlined />,
-            onPress: handleTabs
-        },
-    ]
-
     const renderLocations = (locations) => {
         return locations.map(location => {
             return (
@@ -126,12 +54,6 @@ const LocationsList = ({ locations }) => {
     return (
         <>  
             {testLocations && renderLocations(testLocations)}
-            <LocationDetails
-                visible={detailsVisible}
-                onClose={handleDetailsClose}
-                location={testData}
-                tabs={detailsTabs}
-            />
         </>
     );
 };
--- client/src/pages/Locations/LocationsScreen/index.js
+++ client/src/pages/Locations/LocationsScreen/index.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
-import Icon, { KeyOutlined, UnorderedListOutlined } from "@ant-design/icons";
+import Icon, {
+    KeyOutlined,
+    UnorderedListOutlined,
+    CloseCircleOutlined
+} from "@ant-design/icons";
 
 import { ReactComponent as MapIcon } from "../../../assets/icons/map.svg"; 
 import { ReactComponent as FilterIcon } from "../../../assets/icons/filters.svg";
+import { ReactComponent as GoogleMapsIcon } from "../../../assets/icons/google-maps.svg";
+import { ReactComponent as BookmarkIcon } from "../../../assets/icons/bookmark.svg";
 
 import CircleIconBtn from "../../../components/CircleIconBtn";
 import SearchBar from "../../../components/SearchBar";
@@ -10,20 +16,92 @@ import SearchBar from "../../../components/SearchBar";
 import LocationsMap from "../LocationsMap";
 import LocationsList from "../LocationsList";
 import LocationsFilter from "../LocationsFilters";
+import LocationDetails from "../../../components/LocationDetails";
 
 import { Layout, Top } from "./styled";
 
 const LocationsScreen = () => {
     const [filtersVisible, setFiltersVisible] = useState(false);
+    const [detailsVisible, setDetailsVisible] = useState(false);
     const [mapView, setMapView] = useState(false);
+
+    const handleFilterToggle = () => {
+        setFiltersVisible(!filtersVisible);
+    }
+
+    const handleDetailsOpen = () => {
+        setDetailsVisible(true);
+    }
+
+    const handleDetailsClose = () => {
+        setDetailsVisible(false);
+    }
 
     const handleViewToggle = () => {
         setMapView(!mapView);
     }
 
-    const handleFilterToggle = () => {
-        setFiltersVisible(!filtersVisible);
+    const handleTabs = (tab) => {
+        switch (tab) {
+            case "directions":
+                // Redirect to GMaps
+                break;
+            case "bookmark":
+                // Add to Bookmarks
+                break;
+            case "details":
+                setDetailsVisible(true);
+                break;
+            case "close":
+                setDetailsVisible(false);
+        }
     }
+
+    const testData = {
+        name: "Telus World of Science",
+        distance: 5,
+        bookmarked: true,
+        visited: false,
+        details: {
+            type: "Museum / Gallery",
+            address: "1455 Quebec St, Vancouver, BC V6A 3Z7",
+            hours: {
+                status: "Open now",
+                days: {
+                    mon: ["08:00 AM", "10:00 PM"],
+                    tue: ["08:00 AM", "10:00 PM"],
+                    wed: ["08:00 AM", "10:00 PM"],
+                    thu: ["08:00 AM", "10:00 PM"],
+                    fri: ["08:00 AM", "10:00 PM"],
+                    sat: [],
+                    sun: [],
+                }
+            },
+            phone: "6041234567",
+            website: "https://www.scienceworld.ca"
+        }
+    }
+
+    const detailsTabs = [
+        {
+            key: "directions",
+            name: "Get Directions",
+            icon: <Icon component={GoogleMapsIcon}/>,
+            onPress: handleTabs
+        },
+        {
+            key: "bookmark",
+            name: `${testData.bookmarked ? "Remove from" : "Add to"} Bookmarks`,
+            icon: <Icon component={BookmarkIcon}/>,
+            onPress: handleTabs
+        },
+        {
+            key: "close",
+            name: "Close",
+            icon: <CloseCircleOutlined />,
+            onPress: handleTabs
+        },
+    ]
 
     return (
         <Layout>
@@ -41,12 +119,26 @@ const LocationsScreen = () => {
                 onClose={handleFilterToggle}
             />
 
-            {mapView ? <LocationsMap /> : <LocationsList />}
+            <LocationDetails
+                visible={detailsVisible}
+                onClose={handleDetailsClose}
+                location={testData}
+                tabs={detailsTabs}
+            />
+
             <CircleIconBtn
                 className="view-toggle"
                 icon={mapView ? <UnorderedListOutlined /> : <Icon component={MapIcon} />}
                 onClick={handleViewToggle}
             />
+
+            {mapView 
+                ? <LocationsMap /> 
+                :
+                <LocationsList
+                    handleTabs={handleTabs}
+                    handleDetailsOpen={handleDetailsOpen}
+                />}
         </Layout>
     );
 };
