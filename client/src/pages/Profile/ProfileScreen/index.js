a222c643598b9e72466668a010bf6b0fd7730969
Ethan Lee <33e.lee@gmail.com>
2021-05-18 09:34:05 -0700

    fixed conflicts in Register and ProfileScreen

--- client/src/pages/Profile/ProfileScreen/index.js
+++ client/src/pages/Profile/ProfileScreen/index.js
@@ -5,39 +5,21 @@ import PointsSection from "../components/Points/PointsSection";
 import LocationsList from "../components/History/LocationsList";
 
 const ProfileScreen = () => {
-<<<<<<< HEAD
-    const { logout } = useAuth();
+    //const { logout } = useAuth();
     const history = useHistory();
 
     function handleLogout() {
-        logout();
+        //logout();
         history.push("/login");
     }
 
     return (
         <div>
-            <h1>Profile page</h1>
-            Email: Some email
-            <button onClick={handleLogout}>Log Out</button>
+            <AvatarSection />
+            <PointsSection points={12} />
+            <LocationsList />
         </div>
     );
-=======
-  //const { logout } = useAuth();
-  const history = useHistory();
-
-  function handleLogout() {
-    //logout();
-    history.push("/login");
-  }
-
-  return (
-    <div>
-      <AvatarSection/>
-      <PointsSection points={12}/>
-      <LocationsList/>
-    </div>
-  );
->>>>>>> 07e7eb60c287a6dae2656b1b15c874ad1b778f55
 };
 
 export default ProfileScreen;
