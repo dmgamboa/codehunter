43df33dda517c4e45e88c8c85de181d6832645a1
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-05-19 21:01:38 -0700

    fix subpage blank page bug

--- client/src/pages/Account/index.js
+++ client/src/pages/Account/index.js
@@ -2,14 +2,13 @@ import { Route, Switch, useRouteMatch } from "react-router-dom";
 
 import Login from "./Login/index";
 import Register from "./Register/index";
-import AuthProvider from "../../context/Auth";
 
 const Account = () => {
     const match = useRouteMatch();
     return (
         <Switch>
-            <Route exact path={`${match.path}/register`} render={() => <Register />} />
-            <Route exact path={`${match.path}/login`} render={() => <Login />} />
+            <Route path={`${match.path}/`} render={() => <Login />} />
+            <Route path={`${match.path}/register`} render={() => <Register />} />
         </Switch>
     );
 };
