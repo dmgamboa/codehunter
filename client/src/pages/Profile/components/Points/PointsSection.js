2b474688e9a90de11c850ad52806d29b993f4588
PariyaKasaeian <pkasaiyan@gmail.com>
2021-05-19 20:30:22 -0700

    Fixes Points Section's styling.

--- client/src/pages/Profile/components/Points/PointsSection.js
+++ client/src/pages/Profile/components/Points/PointsSection.js
@@ -1,7 +1,9 @@
 import PSStyle from "./PointsSectionStyle";
-import Liner from "../../images/blueDivider.png";
 import React, { useState } from "react";
-import { Modal, Button, message } from "antd";
+import theme from "../../../../context/themes/main";
+
+const { colors } = theme;
+
 
 //pass 0 to points when redeemed
 function PointsSection({ points }) {
@@ -11,17 +13,17 @@ function PointsSection({ points }) {
             <PSStyle>
                 <div className="content">
                     <hr id="linerTop"/>
-                        <div id="digits">
-                            <p>
-                                <span
-                                style={{ color: "#005526", fontWeight: "700", fontSize: "280%" }}
-                                className="points-digits">
-                                    {points}
-                                </span>
-                                <br/>
-                                <span style={{fontSize:"150%", marginLeft:"15px"}}> points</span>
-                            </p>
-                        </div>
+                    <div id="digits">
+                        <p>
+                            <span
+                            style={{ color: colors.secondary, fontWeight: "700", fontSize: "280%" }}
+                            className="points-digits">
+                                {points}
+                            </span>
+                            <br/>
+                            <span style={{fontSize:"150%", marginLeft:"15px"}}> points</span>
+                        </p>
+                    </div>
                     <hr id="linerBottom"/>
                 </div>
             </PSStyle>
