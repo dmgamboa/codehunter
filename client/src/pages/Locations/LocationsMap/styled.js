1d829a86d81749b6743c20f434af969e666e5efa
Donna Dominguez Gamboa <dm.dominguezgamboa@gmail.com>
2021-05-25 08:20:25 -0700

    generate map marker based on locationType

--- client/src/pages/Locations/LocationsMap/styled.js
+++ client/src/pages/Locations/LocationsMap/styled.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import theme from "../../../context/themes/main";
+
+const { colors } = theme;
 
 export const StyledMap = styled.div`
     z-index: 0;
@@ -11,5 +14,11 @@ export const StyledMap = styled.div`
     .gmnoprint,
     .gm-style-iw-d {
         display: none;
+    }
+
+    .marker {
+        font-size: 2rem;
+        color: ${colors.primary};
+        filter: drop-shadow(1px 2px 2px rgba(0,0,0,0.3 ));
     }
 `;
