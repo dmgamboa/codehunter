d87d63989024208a6f98b1b7cd4599c88e77e31d
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-05-25 15:15:02 -0700

    Formatted the date.

--- client/src/pages/Profile/components/History/Location.js
+++ client/src/pages/Profile/components/History/Location.js
@@ -1,13 +1,18 @@
 const renderHistoryList = (list) => {
-    console.log(list);
     return list.map((item) => {
         const { _id, location, date, points } = item;
-
+        const newDate = new Date(date);
+        const year = newDate.getFullYear();
+        const month = newDate.getMonth() + 1;
+        const day = newDate.getDate();
+        
         return (
             <div key={_id}>
                 <p>
                     <span className="location-name">{location}</span>
-                    <span className="location-date">{date}</span>
+                    <span className="location-date">
+                        {year + "-" + month + "-" + day}
+                    </span>
                     <span className="location-points">{points}</span>
                 </p>
             </div>
