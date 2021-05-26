d54ce0cf6fb84795f1744b3498e28065a4e45878
PariyaKasaeian <pkasaiyan@gmail.com>
2021-05-26 12:36:30 -0700

    Fixes Edit Button.

--- client/src/pages/Profile/components/Avatar/Avatar.js
+++ client/src/pages/Profile/components/Avatar/Avatar.js
@@ -1,7 +1,7 @@
 import { useHistory } from "react-router-dom";
 import AvatarContainer from "./AvatarStyled";
 import pic from "../../images/sampleAvatar.png";
-import EditButton from "../EditBtn"
+import EditButton from "../EditBtn";
 
 const AvatarSection = ({firstName, lastName, avatar}) => {
     const history = useHistory();
