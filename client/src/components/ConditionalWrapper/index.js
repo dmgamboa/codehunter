7e83e31f71c2bfe73c334fb8a30968bf3c6d333a
Joshua Krauchi <jkrauchi@my.bcit.ca>
2021-05-27 14:42:36 -0700

    Moved folders.

--- client/src/components/ConditionalWrapper/index.js
+++
@@ -1,7 +0,0 @@
-// From https://blog.hackages.io/conditionally-wrap-an-element-in-react-a8b9a47fab2
-
-const ConditionalWrapper = ({ condition, wrapper, children }) => {
-    return <>{condition ? wrapper(children) : children}</>;
-};
-
-export default ConditionalWrapper;
--- client/src/pages/Locations/LocationsList/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import InfiniteScroll from "react-infinite-scroll-component";
-
-export const StyledInfiniteScroll = styled(InfiniteScroll)`
-    .ant-card {
-        margin-bottom: 1rem;
-    }
-`;
